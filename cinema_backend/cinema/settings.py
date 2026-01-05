9b0b091fa4131fa9d5a724caaa1ee79427ab3235
xeatteam <xeatteam@gmail.com>
2026-01-05 18:58:30 +0100

    dernier push234

--- cinema_backend/cinema/settings.py
+++ cinema_backend/cinema/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'cine-allo.onrender.com', 
