0f8d636ca472e7501d6883b8025da322aa107db5
xeatteam <xeatteam@gmail.com>
2026-01-05 20:56:20 +0100

    dernier push23456

--- cinema_backend/cinema/settings.py
+++ cinema_backend/cinema/settings.py
@@ -102,7 +102,7 @@ LOGIN_REDIRECT_URL = 'https://cine-allo.onrender.com'
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = 'https://cine-allo.onrender.com/login'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
