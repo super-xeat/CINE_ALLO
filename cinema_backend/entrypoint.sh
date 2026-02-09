05e1eb4e2557937ef70f1bb32a1b97100c00634e
xeatteam <xeatteam@gmail.com>
2026-02-09 19:26:52 +0100

    trucs

---
+++ cinema_backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# On quitte immédiatement si une commande échoue
+set -e
+
+echo "--- Application des migrations sur Neon ---"
+python manage.py migrate --noinput
+
+echo "--- Collecte des fichiers statiques ---"
+python manage.py collectstatic --noinput
+
+echo "--- Démarrage de Gunicorn ---"
+# Remplace 'mon_projet' par le nom du dossier contenant ton wsgi.py
+exec gunicorn mon_projet.wsgi:application --bind 0.0.0.0:10000
